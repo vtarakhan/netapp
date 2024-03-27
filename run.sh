83871916b39a11154c1ae736dad06aceca3fd4c8
vtarakhan <v.tarakhan@kasta.ua>
2024-03-27 20:10:45 +0200

    fix chromium crashes

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 xhost +
-docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix vtarahan/netapp:java8
+docker run -it --rm --shm-size 1000000000 -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix vtarahan/netapp:java8
 xhost -
